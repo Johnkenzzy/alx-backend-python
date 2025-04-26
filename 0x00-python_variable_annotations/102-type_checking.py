2baf6897a23576603c4ab760b308378343cb58ae
johnkenzzy <johnkenumeh@gmail.com>
2025-04-26 05:33:53 +0100

    Advanced task2 | mypy-compliant

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Defines the zoom_array function"""
+from typing import List, Sequence, Any
+
+
+def zoom_array(lst: Sequence[Any], factor: int = 2) -> List[Any]:
+    """Return the appropiate collection type"""
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)  # Fixed: pass an int, not a float
