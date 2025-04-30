feebc1604a66b43c78feb98a440ff367550827ea
Johnkenzzy <johnkenumeh@gmail.com>
2025-04-30 04:59:22 +0100

    Fix import

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Collects 10 random numbers from an async generator"""
 from typing import List
-from 0-async_generator import async_generator
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Measures runtime of running async_comprehension 4 times in parallel"""
+import asyncio
+import time
+from 1-async_comprehension import async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Runs async_comprehension 4 times concurrently and returns runtime"""
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+    return end_time - start_time
