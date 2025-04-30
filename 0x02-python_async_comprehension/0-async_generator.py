e184047dc4f9f1bfad1f3f3d150835fc22d7be33
Johnkenzzy <johnkenumeh@gmail.com>
2025-04-30 06:39:35 +0100

    Fix anotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Defines the async_generator coroutine"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Makes asynchronous loop"""
     for _ in range(10):
         await asyncio.sleep(1)
